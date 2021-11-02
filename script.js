51dfe8da01ea58aad628a3c748090a1fd0ca75df
nico <nicogcarbone@gmail.com>
2021-11-01 21:21:50 -0300

    codigo identado

--- script.js
+++ script.js
@@ -7,20 +7,12 @@ let formaDepago = prompt(
     "Nos gustaria saber que medios de pago utiliza al visitar nuestros locales: \n A (Efectivo), \n B (Tarjeta de Credito) \n C (Mercado Pago)"
 );
 
-if ((formaDepago === "A") || (formaDepago === "a")) {
+if (formaDepago === "A" || formaDepago === "a") {
   console.log("en efectivo puede obtener hasta un 10% de descuento");
-} 
-
-
-else if ((formaDepago === "B") || (formaDepago === "b")) {
+} else if (formaDepago === "B" || formaDepago === "b") {
   console.log("Con su tarjeta puede acceder a todas los planes de pago");
-} 
-
-else if ((formaDepago === "C") || (formaDepago === "c")) {
+} else if (formaDepago === "C" || formaDepago === "c") {
   console.log("con Mercado Pago sumara puntos para su proxima compra");
-} 
-
-
-else {
-        alert("No es un medio de pago valido en nuestros locales");
+} else {
+  alert("No es un medio de pago valido en nuestros locales");
 }
