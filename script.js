85869f5de1abbd3028d0f4cb66876df61496a1e1
nico <nicogcarbone@gmail.com>
2021-11-03 19:15:29 -0300

    trabajo nro3 ciclos/iteraciones

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-let nombreIngresado = prompt("Bienvenido; solicitmos tu nombre");
-let formaDepago = prompt(
-  "gracias" +
-    " " +
-    nombreIngresado +
-    " " +
-    "Nos gustaria saber que medios de pago utiliza al visitar nuestros locales: \n A (Efectivo), \n B (Tarjeta de Credito) \n C (Mercado Pago)"
-);
+for (let i = 0; i < 20; i++) {
+  if (i > 10) breack;
 
-if (formaDepago === "A" || formaDepago === "a") {
-  console.log("en efectivo puede obtener hasta un 10% de descuento");
-} else if (formaDepago === "B" || formaDepago === "b") {
-  console.log("Con su tarjeta puede acceder a todas los planes de pago");
-} else if (formaDepago === "C" || formaDepago === "c") {
-  console.log("con Mercado Pago sumara puntos para su proxima compra");
-} else {
-  alert("No es un medio de pago valido en nuestros locales");
+  let nombre = prompt("ponga su nombre y tendra su numero de asiento");
+
+  if (nombre) {
+    alert(i);
+  } else if (nombre === "") {
+    alert("no a ingresado su nombre");
+    breack;
+  }
 }
