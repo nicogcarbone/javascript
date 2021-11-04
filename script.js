1166d0108ed4127f020ce0cd1c772592807ebd27
nico <nicogcarbone@gmail.com>
2021-11-04 00:15:59 -0300

    cambie la propiedad break, hize lo posible para que no tome numeros pero no encontre la manera correctao por lo menos creo que no lo vimos

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
+//trabajo nro 3//
+
 for (let i = 0; i < 20; i++) {
-  if (i > 10) breack;
+  if (i > 10) break;
 
   let nombre = prompt("ponga su nombre y tendra su numero de asiento");
 
-  if (nombre) {
+  if (
+    nombre != "" &&
+    nombre != "1" &&
+    nombre != "2" &&
+    nombre != "3" &&
+    nombre != "4" &&
+    nombre != "5" &&
+    nombre != "6" &&
+    nombre != "7" &&
+    nombre != "8" &&
+    nombre != "9" &&
+    nombre != "10" &&
+    nombre != "0" &&
+    nombre != "123" &&
+    nombre != "1234" &&
+    nombre != "12345" &&
+    nombre != "123456" &&
+    nombre != "1234567" &&
+    nombre != "111" &&
+    nombre != "0"
+  ) {
     alert(i);
-  } else if (nombre === "") {
+  } else {
     alert("no a ingresado su nombre");
-    breack;
   }
 }
