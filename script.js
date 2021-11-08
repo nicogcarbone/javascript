cf35dc3757151a5166b3d9c71f32e306726606d4
nico <nicogcarbone@gmail.com>
2021-11-08 19:50:13 -0300

    estrega DE FUNCIONES

--- script.js
+++ script.js
@@ -1,41 +1,20 @@
-//trabajo nro 3//
 
-for (let i = 0; i < 20; i++) {
-  if (i > 10) break;
 
-  let nombre = prompt("ponga su nombre y tendra su numero de asiento");
 
-  if (
-    nombre != "" &&
-    nombre != "1" &&
-    nombre != "2" &&
-    nombre != "3" &&
-    nombre != "4" &&
-    nombre != "5" &&
-    nombre != "6" &&
-    nombre != "7" &&
-    nombre != "8" &&
-    nombre != "9" &&
-    nombre != "10" &&
-    nombre != "0" &&
-    nombre != "123" &&
-    nombre != "1234" &&
-    nombre != "12345" &&
-    nombre != "123456" &&
-    nombre != "1234567" &&
-    nombre != "111" &&
-    nombre != "0"
-  ) {
-    alert(i);
-  } 
+function resultado(textoUno, textoDos) {
+  textoUno = prompt("ingrese primer valor");
+  textoDos = prompt("ingrese segundo valor");
+  textoDos = parseInt (textoDos);
+  textoUno= parseInt(textoUno);
+  let mensaje = textoUno + textoDos;
+  console.log("PRECIOS SIN IVA" + " " + "$" + mensaje);
 
-  
-  else {
-    alert("no a ingresado su nombre");
-  }
-
-   if (nombre !=="")  {
-    console.log ("hola" + " " + nombre + " " + "tu asiento es" + " " , i)
+  if (mensaje !== "") {
+    console.log("IVA INCLUIDO" + " " + "$" + mensaje * 1.21);
   }
 }
+
+resultado (0,0 );
+
+
 
