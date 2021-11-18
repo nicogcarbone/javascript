40e147ca0088261623404d110cad62465d7f92eb
nico <nicogcarbone@gmail.com>
2021-11-17 22:49:22 -0300

    entrega de arrays y metodos

--- script.js
+++ script.js
@@ -1,78 +1,33 @@
-/*
-alert(
-  "BIENVENIDO A TU TORNEO.COM \n A CONTINUACION INGRESA TUS EQUIPOS Y PUNTAJES \n LUEGO LO VERAS REFLEJADO EN LA TABLA"
-);
-alert("EL TORNEO CONTIENE 4 EQUIPOS Y PODES ELEGIR EL NOMBRE QUE QUIERAS");
 
-for (i = 0; i < 8; i++) {
-  if (i > 4) {
+
+let equipo = [];
+let puntaje = [];
+
+let cantidad = 4;
+
+do {
+  let entrada = prompt("ingrese equipo");
+  equipo.push(entrada);
+
+  let puntos = parseInt(prompt("ingrese puntos"));
+  puntaje.push(puntos + " pts");
+
+  while (entrada === "" || puntos === "") {
+    alert("NO INGRESO DATOS");
     break;
   }
-  let equipo = prompt("Ingrese Equipo");
-  let puntos = parseInt(prompt("Gano 3,Empato 1 y Perdio 0"));
+} while (equipo.length != cantidad && puntaje.length != cantidad);
 
-  console.log(equipo + " " + puntos + "pts");
+console.log(equipo, puntaje);
 
-  while (equipo === "" || puntos === "") {
-    console.log("NO INGRESO DATOS");
-    alert("NO INGRESO DATOS");
+const inscripcion = [
+  { nombreTorneo: "el bidon $ ", precio: 2800 },
+  { nombreTorneo: "torneo tsubasta $ ", precio: 2500 },
+];
 
-    if (equipo !== "" || puntos !== "") break;
-  }
+for (const inscripciones of inscripcion) {
+  const iva = (x) => x * 1.21;
+  let total = iva(inscripciones.precio);
+
+  console.log(inscripciones.nombreTorneo,total);
 }
-
-function datos() {
-  benef = console.log(
-    "Ahora que ingresaste todo disfruta de los beneficios de tener tu Torneo organizado TU CLAVE DE ORGANIZADOR ES contra"
-  );
-  contra =
-    "TENES TU DESCUENTO EN LOS PRIMEROS 5 TORNEOS QUE INGRESES  Y PODES SORTEAR UN JUEGO DE CAMISETAS A ELECCION";
-  if (contra === "contra") {
-    alert(contra);
-  }
-}
-
-datos();
-*/
-class equipos {
-  constructor(equipo, puntos) {
-    this.equipo = equipo;
-    this.puntos = parseInt(puntos);
-  }
-
-  datos() {
-    for (let e = 0; e < 4; e++)  {
-      this.equipo = prompt("ingresa equipo");
-      this.puntos = prompt("ingesa puntaje");
-
-      console.log(this.equipo + " " + this.puntos + "pts");
-
-      while (this.equipo === "" || this.puntos === "") {
-        console.log("NO INGRESO DATOS");break
-      
-      }
-    }
-  }
-}
-
-
-
-
-const equipo = new equipos();
-equipo.datos();
-
-class inscripciones {
-  constructor(torneo, precio) {
-    this.torneo = torneo;
-    this.precio = parseFloat(precio);
-  }
-
-  sumarIva() {
-    this.precio = this.precio * 1.21;
-  }
-}
-const torneos = new inscripciones ("EL BIDON", "2066,15");
-
-torneos.sumarIva();
-
-console.log(torneos);
